825b7ad714a8cb85564dafcc0b322d04a632eb8e
zouber04 <i.zouber04@gmail.com>
2022-07-08 11:59:55 -0400

    a2p1: Working Second Version a2p1.py

--- a2p1.py
+++ a2p1.py
@@ -4,16 +4,72 @@
 # Date july 8 2022
 
 def get_name():
-    pass
+    """
+    Args:
+        -
+    Returns: String, customer name
+    """
+    name = input("Name: ")
+    return name
 
-def disp_menu():
-    pass
+def disp_menu(products):
+    """
+    Args: tuple of product name and price
+        -
+    Returns: String, menu
+    """
+    menu = ""
+    for x in range(len(products)):
+        menu += str(x+1) + products[x][0] + "($"+str(products[x][1])+")"
 
-def is_select_valid():
+    return menu
+
+def get_product(products, selection):
+     """
+    Args: Integer, selection
+        -
+    Returns: String,
+    """
+
+def is_select_valid(selection):
+    """
+    Args: Integer, selection
+        -
+    Returns: Boolean
+    """
+
     pass
 def get_num_product():
+    """
+    Args: 
+        -
+    Returns: Integer, Number of products
+    """
     pass
 
 def display_receipt():
+    """
+    Args: String, Product Integer, Number of product Intege, Cost
+        -
+    Returns: None
+    """
     pass
 
+def start_store(products):
+    """
+    Args: list of tuples of products
+        -
+    Returns: 
+    """
+    name = get_name()
+    
+
+
+if __name__ == "__main__":
+    products = [
+        ("Desktop Computer",850), 
+        ("LAPTOP COMPUTER", 1225),
+        ("Tablet", 600),
+        ("Toaster Oven", 85)
+    ]
+    start_store(products)
