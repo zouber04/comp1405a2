491d83badb5815428d74338823c6290c00908c36
zouber04 <i.zouber04@gmail.com>
2022-07-08 11:08:32 -0400

    Initial version a2p1.py

---
+++ a2p1.py
@@ -0,0 +1,19 @@
+###################
+# Author: Zouber Ismail
+# Assignment 2 Problem 1
+# Date july 8 2022
+
+def get_name():
+    pass
+
+def disp_menu():
+    pass
+
+def is_select_valid():
+    pass
+def get_num_product():
+    pass
+
+def display_receipt():
+    pass
+
