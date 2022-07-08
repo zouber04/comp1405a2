2c5feadd48abf64064e57e367c1c714ff91dc1e4
zouber04 <i.zouber04@gmail.com>
2022-07-08 13:52:43 -0400

    a2p1, FIX: Completed version, fixed comment format and structure

--- a2p1.py
+++ a2p1.py
@@ -1,63 +1,65 @@
-###################
+#########################
 # Author: Zouber Ismail
 # Assignment 2 Problem 1
 # Date july 8 2022
+#########################
 
 def get_name():
     """
-    Args:
-        -
+    Gets customer name
+    Args: None
     Returns: String, customer name
+
     """
     name = input("Name: ")
     return name
 
 def disp_menu(products):
     """
-    Args: tuple of product name and price
-        -
-    Returns: String, menu
+    Displays and prints menu
+    Args: 
+        - List[Tuple(str, int)], list of pairs of product names and prices
+    Returns: None
+
     """
     menu = ""
     for x in range(len(products)):
         menu += str(x+1)+". " + products[x][0] + " ($"+str(products[x][1])+")"+"\n"
-
     print(menu)
 
-def get_product(products, selection):
-     """
-    Args: Integer, selection
-        -
-    Returns: String,
-    """
-def get_selection():
 
+def get_selection():
+    """
+    Request item selection number from customer
+    Args: None
+    Returns: int, selection number
+
+    """
     selection = int(input("> "))
     return selection
+
 def is_select_valid(products, selection):
     """
-    Args: Integer, selection
-        -
-    Returns: Boolean
+    Validates customer selection
+    Args:
+        - List[Tuple(str, int)], list of pairs of product names and prices
+        - int, selection number
+    Returns: bool, describes wether selection number is valid
 
     """
-
-
-    if(0 < selection <= len(products)):
+    if 0 < selection <= len(products):
         return True
-
-    else:
-        print("Im Sorry, thats not a Valid selection. Please Enter a selection from 1-4\n")
-        
-        return False
-
-
+    print("Im Sorry, thats not a Valid selection. Please Enter a selection from 1-4\n")
+    return False
 
 def get_num_product(products, selection):
     """
+    Get the number of a product, identified by the selection number, requested by the customer
     Args: 
-        -
-    Returns: Integer, Number of products
+        - List[Tuple(str, int)], list of pairs of product names and prices
+        - int, selection number
+    Returns: int, number of products requested by customer
+
     """
     is_valid = False
     while not is_valid:
@@ -70,9 +72,14 @@ def get_num_product(products, selection):
 
 def display_receipt(cust_name, num_product, product_name, product_price):
     """
-    Args: String, Product Integer, Number of product Intege, Cost
-        -
+    Display and prints customer receipt
+    Args: 
+        - str, customer name
+        - int, number of products requested
+        - str, product name
+        - int, product price 
     Returns: None
+
     """
     
     receipt = \
@@ -89,27 +96,22 @@ def display_receipt(cust_name, num_product, product_name, product_price):
 
 def start_store(products):
     """
-    Args: list of tuples of products
-        -
-    Returns: 
+    Starts the store interface
+    Args: 
+        - List[Tuple(str, int)], list of pairs of product names and prices
+    Returns: None
+
     """
+    is_valid = False
     name = get_name()
-    disp_menu(products)
-    
 
-    selection = get_selection()
-    is_valid = is_select_valid(products,selection)
-
-    while is_valid == False:
+    while not is_valid:
         disp_menu(products)
         selection = get_selection()
         is_valid = is_select_valid(products, selection)
 
-    
     num = get_num_product(products,selection) 
-    
     display_receipt(name, num, products[selection-1][0],products[selection-1][1])
-
 
 if __name__ == "__main__":
     products = [
